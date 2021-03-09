0a2afde58210ebe9fcee26fe828b54ea585f9d66
piyushtsr <piyushrahangdale21@gmail.com>
2021-03-09 22:19:51 +0530

    updated profile route with delete profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -133,6 +133,28 @@ router.get(
   }
 );
 
+// @route    DELETE api/profile
+// @desc     Delete profile, user & posts
+// @access   Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    // Remove user posts
+    // Remove profile
+    // Remove user
+    await Promise.all([
+      Post.deleteMany({ user: req.user.id }),
+      Profile.findOneAndRemove({ user: req.user.id }),
+      User.findOneAndRemove({ _id: req.user.id })
+    ]);
+
+    res.json({ msg: 'User deleted' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+
 
 
 
